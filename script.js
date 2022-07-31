a49c7f2a477aec20f5dfa042a5f6d46afeb00a73
Nizami Sevindi <nsevindi87@gmail.com>
2022-07-31 13:58:42 +0200

    go on

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-const fileInput = document.querySelector(".file-input");
-chooseImgBtn = document.querySelector(".choose-img");
-previewImg = document.querySelector(".preview-img img");
-filterOptions = document.querySelector(".filter button");
+const fileInput = document.querySelector(".file-input"),
+filterOptions = document.querySelectorAll(".filter button"),
+previewImg = document.querySelector(".preview-img img"),
+chooseImgBtn = document.querySelector(".choose-img"),
+filterSlider = document.querySelector(".filter-info .input"),
+filterName = document.querySelector(".filter-info .name");
 
 
-const loadImage = () => {         //3
-    let file = fileInput.files[0] //getting user selected file
-    if(!file) return;       // return if user hasnt selected file
+const loadImage = () => {                           //3
+    let file = fileInput.files[0];                 //getting user selected file
+    if(!file) return;                              // return if user hasnt selected file
     previewImg.src = URL.createObjectURL(file);   //passing file url as preview img src
     previewImg.addEventListener("load", ()=>{     //resim secildikten sonra pasif olan butonlar aktif hale geliyor
-        document.querySelector(".container").classList.remove("disable")
+        document.querySelector(".container").classList.remove("disable");
     })
 }
 
-filterOptions.forEach(option => {                 // 4
-    option.addEventListener("click", () => {      //adding click event listener to all filter buttons
-        document.querySelector(".filter .active").classList.remove(".active")   // active class siliniyor
-        option.classList.add(".active")    // tiklanana active classi ataniyor
-    });
-});
+filterOptions.forEach(option => {          //4
+    option.addEventListener("click", () => {
+        document.querySelector(".active").classList.remove("active");
+        option.classList.add("active");
+        filterName.innerText = option.innerText
+    })
 
+}) 
 
-fileInput.addEventListener("change", loadImage) // 2
+const updateFilter = ()=>{
+    console.log(filterSlider.value)
+}
 
+fileInput.addEventListener("change", loadImage); // 2
+filterSlider.addEventListener("input", updateFilter); //5
 chooseImgBtn.addEventListener("click", () => fileInput.click());  // 1
 
 
